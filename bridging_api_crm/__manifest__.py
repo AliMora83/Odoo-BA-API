b4979b4e402239e7ed90d766edcd27850683cb7f
Ali Mora <ali@openmindi.co.za>
2025-12-04 14:35:29 +0200

    [IMP] New Module  added

---
+++ bridging_api_crm/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "API : CRM : Script",
+    "version": "18.0.0.1",
+    "category": "",
+    "summary": """Bridging Africa : CRM API Script""",
+    "description": "Bridging Africa",
+    "author": "Bridging",
+    "website": "",
+    "company": "#",
+    "license": "AGPL-3",
+    "sequence": 1,
+    "depends": [
+        "base",
+        "crm",
+    ],
+    "data": [
+        "data/data.xml",
+        "security/ir.model.access.csv",
+        "views/crm_lead.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ bridging_api_crm/models/crm.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+import logging
+import time
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, Command
+import requests
+
+
+class ServiceService(models.Model):
+    _name = "service.service"
+
+    name = fields.Char("Name")
+
+
+class CrmLead(models.Model):
+    _inherit = "crm.lead"
+
+    quote_id = fields.Integer(
+        "Quote ID",
+    )
+    provider_id = fields.Char(
+        "Provider ID",
+    )
+    ispaid = fields.Boolean(
+        "IsPaid",
+    )
+    workerrrgency = fields.Char(
+        "WorkerUrgency",
+    )
+    q_status_id = fields.Char(
+        "StatusID",
+    )
+    service_id = fields.Many2one(
+        "service.service",
+        string="Service Name",
+    )
+
+    @api.model
+    def _cron_api_crm_services(self):
+        URL = "https://bridgingafrica.ncnsoftware.com/api/odooapi/GetAllRequests"
+        Req = requests.get(URL)
+        print(
+            "ReqReq",
+            Req.json(),
+            Req.status_code,
+            type(Req.status_code),
+        )
+        LeadObj = self.env["crm.lead"]
+        if Req.status_code == 200:
+            print("-------")
+            json_data = Req.json()
+            print("json_datajson_datajson_data", json_data)
+            for data in json_data.get("completed"):
+                if not LeadObj.search(
+                    [
+                        ("quote_id", "=", data.get("Id")),
+                    ]
+                ):
+                    ServiceName = data.get("ServiceName")
+                    service_id = self.env["service.service"].search(
+                        [("name", "=", ServiceName)]
+                    )
+                    if not service_id:
+                        service_id = self.env["service.service"].create(
+                            {"name": ServiceName}
+                        )
+                    LeadObj.create(
+                        {
+                            "quote_id": data.get("Id"),
+                            "provider_id": data.get("ProviderID"),
+                            "contact_name": data.get("Tradesperson"),
+                            "workerrrgency": data.get("Id"),
+                            "ispaid": data.get("IsPaid"),
+                            "q_status_id": data.get("StatusID"),
+                            "name": data.get("WorkerUrgency"),
+                            "partner_name": data.get("Customer"),
+                            "description": data.get("JobDetail"),
+                            "service_id": service_id.id,
+                        }
+                    )
+            for data in json_data.get("pending"):
+                if not LeadObj.search(
+                    [
+                        ("quote_id", "=", data.get("Id")),
+                    ]
+                ):
+                    ServiceName = data.get("ServiceName")
+                    service_id = self.env["service.service"].search(
+                        [("name", "=", ServiceName)]
+                    )
+                    if not service_id:
+                        service_id = self.env["service.service"].create(
+                            {"name": ServiceName}
+                        )
+                    LeadObj.create(
+                        {
+                            "quote_id": data.get("Id"),
+                            "provider_id": data.get("ProviderID"),
+                            "contact_name": data.get("Tradesperson"),
+                            "workerrrgency": data.get("Id"),
+                            "ispaid": data.get("IsPaid"),
+                            "q_status_id": data.get("StatusID"),
+                            "name": data.get("WorkerUrgency"),
+                            "partner_name": data.get("Customer"),
+                            "description": data.get("JobDetail"),
+                            "service_id": service_id.id,
+                        }
+                    )
